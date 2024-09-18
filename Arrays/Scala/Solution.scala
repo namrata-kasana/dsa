0ea46afe40f3548c6cee479228fe65e2c3bfd605
namrata <namrata.kasana@lloydsbanking.com>
2024-09-18 15:52:51 +0530

    scala folder

---
+++ Arrays/Scala/Solution.scala
@@ -0,0 +1,22 @@
+object Solution {
+  def singleNumber(nums: Array[Int]): Int = {
+    val countMap = scala.collection.mutable.HashMap[Int,Int]()
+
+    // convert array to k,v pair
+    nums.foreach (num =>
+      countMap(num) = countMap.getOrElse(num, 0) + 1)
+
+    // Use find to get the number that occurs exactly once
+    countMap.find { case (k, v) => v == 1 } match {
+      case Some((k, _)) => k  // Return the key if found
+      case None => 0          // Return 0 if no single number is found
+    }
+
+
+  }
+  def main(args: Array[String]): Unit = {
+    // Test the singleNumber method
+    val result = singleNumber(Array(1, 2, 2, 3, 3, 4, 4))
+    println(s"The single number is: $result") // Should output: The single number is: 1
+  }
+}
